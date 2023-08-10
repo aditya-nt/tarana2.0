2933e697f1416a906ab179a3ec095a5f319b94ca
Aditya Tripathi <aditya.tripathi@torinit.ca>
2023-08-11 00:55:41 +0530

    Fix(config): Base URL added

--- src/lib/restAPI/index.ts
+++ src/lib/restAPI/index.ts
@@ -1,12 +1,10 @@
 import axios, { AxiosInstance, AxiosResponse, AxiosRequestConfig } from 'axios';
-
-const API_BASE_URL = 'https://itunes.apple.com';
+import { API_BASE_URL } from '@/lib/config';
 
 const APIClient: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
 });
 
-// Define a function to make API requests
 async function makeAPIRequest<T>(method: string, path: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
   try {
     const response: AxiosResponse<T> = await APIClient.request({
